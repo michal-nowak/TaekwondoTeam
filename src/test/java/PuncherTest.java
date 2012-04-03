7fdc50577d82ebed735680e2f0f0b82faee9d511
Michal Nowak <michal.jakub.nowak@gmail.com>
2012-04-04 00:23:29 +0200

    adding first java files

---
+++ src/test/java/PuncherTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: admin
+ * Date: 04.04.2012
+ * Time: 00:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class PuncherTest {
+}

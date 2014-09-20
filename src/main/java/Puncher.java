7d023f5c470aa39452a7d6e22da7983a07166590
Michal Nowak <dollyo@gmail.com>
2014-09-20 11:24:05 +0200

    @minor changing puncher to be better

--- src/main/java/Puncher.java
+++ src/main/java/Puncher.java
@@ -7,7 +7,5 @@
 public class Puncher {
     public static void main(String[] args) {
         System.out.println("ziom");
-        System.out.println("ziom");
-        System.out.println("ziom");
     }
 }
